f8b58ae0fb70ee1e62cefb8b33a9f18f4017a34b
GrigoriyMay <g-mayorov1996@mail.ru>
2022-10-11 16:04:34 +0300

    Сделал второе домашнее задание

--- DZ2/Задача5.py
+++ DZ2/Задача5.py
@@ -1,7 +1,7 @@
 # Реализуйте алгоритм перемешивания списка.
 
 import random
-my_list = [1,2,3,4,5]
+my_list = [1,2,3,4,5,6,7]
 ind_list = []
 
 for i, elem in enumerate(my_list):
@@ -11,7 +11,7 @@ for i, elem in enumerate(my_list):
     a = random.randint(0,len(my_list) -1)
     while a in ind_list:
         a = random.randint(0,len(my_list) -1)
-        if len(ind_list) == 5:
+        if len(ind_list) == len(my_list):
             break
     else:
         ind_list.append(a)
