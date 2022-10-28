6fdf65ed73d3944200fc1dd9c990bc1fc96f4808
GrigoriyMay <g-mayorov1996@mail.ru>
2022-10-28 12:30:10 +0300

    Внёс файл

--- DZ5/Задача1.py
+++ DZ5/Задача1.py
@@ -1,6 +1,6 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв""
 
-with open("file_dz5.1.txt", "r") as text_file:
+with open("file.txt", "r") as text_file:
     text = text_file.read()
 
 res = filter(lambda x: 'абв' not in x,text.split())
