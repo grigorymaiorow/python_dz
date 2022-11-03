4b7038957e31a334ea44b39445a8448905c44386
GrigoriyMay <g-mayorov1996@mail.ru>
2022-11-03 17:21:31 +0300

    Свои изменения

---
+++ Raznoe/Krestiki_noliki__chernovik/Main_program.py
@@ -0,0 +1,30 @@
+import os
+from Zapolnenie_slovarya_sluchayno import filling
+# from prettytable import PrettyTable
+from Paint_table import draw_file
+
+os.system('cls||clear')
+
+draw_file(filling())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
