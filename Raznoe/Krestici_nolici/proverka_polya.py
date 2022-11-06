bcb814e67802ab3cf5ad20be1e4827bce151da80
GrigoriyMay <g-mayorov1996@mail.ru>
2022-11-06 19:34:37 +0300

    Написал функцию проверки вводимого пользователям поля в ветке step_2

---
+++ Raznoe/Krestici_nolici/proverka_polya.py
@@ -0,0 +1,26 @@
+
+def proverka_polya():
+
+    while True:
+
+        # Приглашение к вводу
+        data = input("Введите ячейку в виде [x1y1]: ")
+
+        # Досрочное завершение игры игроком
+        if data == "": 
+            data = input("Вы уверены, что хотети закончить игру. Если да, нажмите [enter]. Если нет, то введите любой символ: ")
+            if data == "":
+                print("Игра закончена.")
+                break
+            else:
+                data = input("Введите ячейку в виде [x1y1]: ")
+
+        # Проверка ввёдённого
+        if len(data) == 4:
+            if data[0] != "x" or not data[1].isdigit() or 3 < int(data[1]) or int(data[1]) < 1 or data[2] != "y" or not data[3].isdigit() or 3 < int(data[3]) or int(data[3]) < 1:
+                print("Вы ввели неверные данные. Попробуйте ещё раз: ")
+            else:
+                data = data
+                break
+        
+    return data
--- Raznoe/Krestici_nolici/step1_module1.py
+++ Raznoe/Krestici_nolici/step1_module1.py
@@ -2,9 +2,11 @@ import os
 from step1_structure_data import filling
 from step1_draw_file import draw_file
 
+
 os.system('cls||clear')
 
 draw_file(filling())
+
 
 
 
